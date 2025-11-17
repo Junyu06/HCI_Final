f0bb8486fe299c8fb6590b2363f04153c5fbc65c
raham24 <rahamriaz@gmail.com>
2025-11-17 14:18:15 -0500

    Added courses tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,6 +24,13 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="courses"
+        options={{
+          title: 'Courses',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="book.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
         name="ratings"
         options={{
           title: 'Ratings',
